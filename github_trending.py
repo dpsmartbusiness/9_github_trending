220f98dadafc50d5574044aaed3a989deb7a5f98
dpsmartbusiness <33832856+dpsmartbusiness@users.noreply.github.com>
2018-01-30 17:07:03 +0500

    add enumerate. fix and rename def show_top_repos_info

--- github_trending.py
+++ github_trending.py
@@ -25,28 +25,32 @@ def get_open_issues_amount(repo_owner, repo_name):
     return len(issues.json())
 
 
-def show_top_repos(top_github_repos):
-    top_number = 0
-    for repo in top_github_repos:
-        top_number += 1
-        repo_name = repo['name']
-        repo_owner = repo['owner']['login']
-        repo_url = repo['url']
-        repo_stars = repo['stargazers_count']
-        print ('#{}\nName: {}\nStars: {}\nIssues: {}\nUrl: {}\n'.format(
-            top_number,
-            repo_name,
-            repo_stars,
-            get_open_issues_amount(
-                repo_owner,
-                repo_name
-            ),
-            repo_url
-        ))
+def show_top_repo_info(top_number, repo_name, repo_owner, repo_url, repo_stars):
+    print('#{}\nName: {}\nStars: {}\nIssues: {}\nUrl: {}\n'.format(
+        top_number,
+        repo_name,
+        repo_stars,
+        get_open_issues_amount(
+            repo_owner,
+            repo_name
+        ),
+        repo_url
+    ))
 
 
 if __name__ == '__main__':
     print('Top 20 of Github repositories by stars for the last week:\n')
     top_size = 20
     top_github_repos = get_trending_repositories(top_size)
-    show_top_repos(top_github_repos)
+    for top_number, repo in list(enumerate(top_github_repos, start=1)):
+        repo_name = repo['name']
+        repo_owner = repo['owner']['login']
+        repo_url = repo['url']
+        repo_stars = repo['stargazers_count']
+        show_top_repo_info(
+            top_number,
+            repo_name,
+            repo_owner,
+            repo_url,
+            repo_stars
+        )
